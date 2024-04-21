de427625cb3d2b02de2099f7a73f6d597a8451ae
Geetha <danda.saigeetha@wipro.com>
2024-04-21 19:03:39 +0000

    added file

---
+++ src/main/java/com/example/mvn_asg/App.java
@@ -0,0 +1,13 @@
+package com.example.mvn_asg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
